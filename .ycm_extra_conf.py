91729f769d2950c1e6fb6308538fa5dbca63a173
minieyes <chenvey2@gmail.com>
2020-10-04 08:53:28 +0800

    add ycm settings

---
+++ .ycm_extra_conf.py
@@ -0,0 +1,14 @@
+flags = [
+    '-Wall',
+    '-Wextra',
+    '-Werror',
+    '-Wno-long-long',
+    '-Wno-variadic-macros',
+    '-fexceptions',
+    '-DNDEBUG',
+    '-std=c++11',
+    '-x',
+    'c++',
+    '-isystem',
+    'C:/Git/mingw64/include/c++/7.3.0',
+]
---
+++ YouCompleteMe.vim
@@ -0,0 +1,2 @@
+let g:ycm_confirm_extra_conf = 0
+let g:ycm_global_ycm_extra_conf='~/.vimrc.d/.ycm_extra_conf.py'
