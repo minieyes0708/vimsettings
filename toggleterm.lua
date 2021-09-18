9a4db98b5b5d9f0d723ebf86410da268f2490a2f
minieyes <chenvey2@gmail.com>
2021-09-18 23:13:44 +0800

    toggleterm default use power shell

--- toggleterm.lua
+++ toggleterm.lua
@@ -9,7 +9,7 @@ require("toggleterm").setup{
     -- persist_size = true,
     -- direction = 'vertical' | 'horizontal' | 'window' | 'float',
     -- close_on_exit = true, -- close the terminal window when the process exits
-    shell = 'bash',
+    shell = 'powershell',
     -- -- This field is only relevant if direction is set to 'float'
     -- float_opts = {
         -- -- The border key is *almost* the same as 'nvim_open_win'
