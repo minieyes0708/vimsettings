18755a9efc3b00e47e4f95b9fb62d4a77f513bca
Minieyes Chen <Minieyes_Chen@novatek.com.tw>
2021-10-05 13:31:26 +0800

    add quickui.projects and user.lua

---
+++ user.lua
@@ -0,0 +1,9 @@
+local this = {}
+
+vim.o.path = './**,,~/.bashrc.d/**,~/.vifm/**,~/.vimrc.d/**,P:/vimwiki/**'
+
+this.vimwiki_path = 'P:/vimwiki/'
+this.sumneko_root_path = 'D:/minieyes_chen/software/lua-language-server'
+this.sumneko_binary_path = 'D:/minieyes_chen/software/lua-language-server/bin/Windows/lua-language-server.exe'
+
+return this
