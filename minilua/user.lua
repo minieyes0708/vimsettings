642705f5dbc680e2a4cfc5d56447c9533a9cbd87
minieyes <chenvey2@gmail.com>
2021-10-11 23:00:21 +0800

    update vimwiki path

--- minilua/user.lua
+++ minilua/user.lua
@@ -1,6 +1,6 @@
 local this = {}
 
-this.vimwiki_path = '~/OneDrive/文件/vimwiki/vimwiki/'
+this.vimwiki_path = '~/OneDrive/文件/vimwiki/'
 this.sumneko_root_path = 'D:/minieyes_chen/software/lua-language-server'
 this.sumneko_binary_path = 'D:/minieyes_chen/software/lua-language-server/bin/Windows/lua-language-server.exe'
 
