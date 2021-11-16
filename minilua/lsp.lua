3ff61aef83d3b61c7e5a9a5476f02b6b222c0ba7
Minieyes Chen <Minieyes_Chen@novatek.com.tw>
2021-11-16 09:06:51 +0800

    add supertab and jdtls

--- minilua/lsp.lua
+++ minilua/lsp.lua
@@ -1,6 +1,6 @@
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'pyright', 'vimls', 'bashls' }
+local servers = { 'pyright', 'vimls', 'bashls', 'jdtls' }
 for _, lsp in ipairs(servers) do
     require'lspconfig'[lsp].setup {
         on_attach = require'minilua.lsp-on_attach'.on_attach,
