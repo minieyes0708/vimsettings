f9ac43c8988d037eff45c1a559050209ddc0eca4
minieyes <chenvey2@gmail.com>
2021-12-17 15:19:03 +0800

    fix sumneko_root_path

--- minilua/user.lua
+++ minilua/user.lua
@@ -7,7 +7,7 @@ if user == 'NVT02863_NB-NVT02863' then
     this.sumneko_binary_path = 'D:/minieyes_chen/software/lua-language-server/bin/Windows/lua-language-server.exe'
 elseif user == 'MINIEYES-PC2-chenv' then
     this.vimwiki_path = '~/OneDrive/文件/vimwiki/'
-    this.sumneko_root_path = 'L:/bin/lua-language-server/bin/Windows'
+    this.sumneko_root_path = 'L:/bin/lua-language-server'
     this.sumneko_binary_path = 'L:/bin/lua-language-server/bin/Windows/lua-language-server.exe'
 end
 
