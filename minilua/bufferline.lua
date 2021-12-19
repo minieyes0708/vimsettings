02e0e3535ff470625872b92e5d4e353b57e39007
Minieyes Chen <chenvey2@gmail.com>
2021-12-19 13:09:23 +0800

    should uppercase buffer id only

--- minilua/bufferline.lua
+++ minilua/bufferline.lua
@@ -1,7 +1,7 @@
 require'bufferline'.setup({
     options = {
         numbers = function(opts)
-            return string.format('%s|%s', opts.raise(opts.ordinal), opts.id)
+            return string.format('%s|%s', opts.ordinal, opts.raise(opts.id))
         end,
     }
 })
