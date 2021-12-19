e208bc2d1c59cf00913160f769e156cc60e413a1
minieyes <chenvey2@gmail.com>
2021-12-19 20:46:02 +0800

    add option for PC2

--- minilua/lsp-omnisharp.lua
+++ minilua/lsp-omnisharp.lua
@@ -4,6 +4,8 @@ local user = vim.env.COMPUTERNAME .. '-' .. vim.env.USERNAME
 
 if user == 'MINIEYESNB1-怡哲' then
     omnisharp_bin = "D:\\minieyes\\software\\omnisharp-win-x64\\OmniSharp.exe"
+elseif user == 'MINIEYES-PC2-chenv' then
+    omnisharp_bin = "L:\\omnisharp-win-x64\\OmniSharp.exe"
 end
 
 require'lspconfig'.omnisharp.setup {
