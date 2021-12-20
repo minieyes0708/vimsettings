66e4355382ca01433eb99e333d90cf984b17b2e3
Minieyes Chen <Minieyes_Chen@novatek.com.tw>
2021-12-20 13:28:13 +0800

    setup omnisharp bin path

--- minilua/lsp-omnisharp.lua
+++ minilua/lsp-omnisharp.lua
@@ -6,6 +6,8 @@ if user == 'MINIEYESNB1-怡哲' then
     omnisharp_bin = "D:\\minieyes\\software\\omnisharp-win-x64\\OmniSharp.exe"
 elseif user == 'MINIEYES-PC2-chenv' then
     omnisharp_bin = "L:\\omnisharp-win-x64\\OmniSharp.exe"
+elseif user == 'MINIEYESNB1-怡哲' then
+    omnisharp_bin = "D:\\minieyes_chen\\software\\Neovim\\share\\nvim\\runtime\\bundle\\YouCompleteMe\\third_party\\ycmd\\third_party\\omnisharp-roslyn\\OmniSharp.exe"
 end
 
 require'lspconfig'.omnisharp.setup {
