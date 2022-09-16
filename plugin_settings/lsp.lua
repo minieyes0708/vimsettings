6c4a298362b9a9cd16b959789414cef7273d98a1
minieyes <chenvey2@gmail.com>
2022-09-17 00:24:32 +0800

    add cssls

--- plugin_settings/lsp.lua
+++ plugin_settings/lsp.lua
@@ -4,7 +4,7 @@
 local capabilities = require'cmp_nvim_lsp'.update_capabilities(
     vim.lsp.protocol.make_client_capabilities()
 )
-local servers = { 'pyright', 'vimls', 'bashls', 'jdtls' }
+local servers = { 'pyright', 'vimls', 'bashls', 'jdtls','cssls' }
 for _, lsp in ipairs(servers) do
     require'lspconfig'[lsp].setup {
         on_attach = require'minilua.lsp-on_attach'.on_attach,
