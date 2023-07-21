10df111d1bd20329d38ba675f3865b5c9de59cf0
minieyes <chenvey2@gmail.com>
2023-07-22 04:28:58 +0800

    com_nvim_lsp.update_capabilities is deprecated

--- plugin_settings/lsp.lua
+++ plugin_settings/lsp.lua
@@ -1,9 +1,7 @@
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
 
-local capabilities = require'cmp_nvim_lsp'.update_capabilities(
-    vim.lsp.protocol.make_client_capabilities()
-)
+local capabilities = require'cmp_nvim_lsp'.default_capabilities()
 local servers = { 'pyright', 'vimls', 'bashls', 'jdtls','cssls' }
 for _, lsp in ipairs(servers) do
     require'lspconfig'[lsp].setup {
