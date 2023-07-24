83556cd678e5bb5fb95313e9f2b50340626b4ac7
Minieyes Chen(陳怡哲) <Minieyes_Chen@novatek.com.tw>
2023-07-24 09:12:31 +0800

    project plugin use telescope to view files

--- minilua/user.lua
+++ minilua/user.lua
@@ -13,6 +13,10 @@ elseif user == 'MINIEYESNB1-怡哲' then
     this.vimwiki_path = '~/OneDrive/文件/vimwiki/'
     this.sumneko_root_path = 'D:/minieyes/bin/lua-language-server'
     this.sumneko_binary_path = 'D:/minieyes/bin/lua-language-server/bin/Windows/lua-language-server.exe'
+elseif user == 'LAPTOP-0U8DPE2D-chenv' then
+    this.vimwiki_path = '~/OneDrive/文件/vimwiki/'
+    this.sumneko_root_path = 'D:/minieyes/software/lua-language-server'
+    this.sumneko_binary_path = 'D:/minieyes/software/lua-language-server/bin/lua-language-server.exe'
 end
 
 this.fzf_with_cb = function(pathname, callback)
@@ -31,14 +35,14 @@ this.open_project = function(pathname)
         this.fzf_with_cb(pathname, 'LuaFuncCommand require"minilua.user".open_project ')
     else
         vim.api.nvim_command('cd ' .. pathname)
-        vim.api.nvim_command('Files')
+        require('telescope.builtin').find_files()
     end
 end
 this.peek_project = function(pathname)
     if pathname == 'D:/minieyes_chen/program' then
         this.fzf_with_cb(pathname, 'LuaFuncCommand require"minilua.user".peek_project ')
     else
-        vim.api.nvim_command('Files ' .. pathname)
+        require('telescope.builtin').find_files({search_dirs={pathname}})
     end
 end
 this.goto_project = function(pathname)
