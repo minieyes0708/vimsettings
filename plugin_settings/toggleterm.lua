a5f93f0caed0474aebd8237eef6deca1d3be8227
Minieyes Chen(陳怡哲) <Minieyes_Chen@novatek.com.tw>
2023-07-25 10:50:18 +0800

    set toggleterm size = 40

--- plugin_settings/toggleterm.lua
+++ plugin_settings/toggleterm.lua
@@ -1,4 +1,5 @@
 require("toggleterm").setup{
+    size = 40,
     open_mapping = [[<c-z>]],
     -- hide_numbers = true, -- hide the number column in toggleterm buffers
     -- shade_filetypes = {},
