710e2e6143351219d9a4f3760d9c0648db7a085a
minieyes <chenvey2@gmail.com>
2023-07-27 10:01:07 +0800

    fix command-palette parsing bug

--- minilua/command-palette.lua
+++ minilua/command-palette.lua
@@ -4,9 +4,11 @@ local this = {}
 
 function this.update()
     local commands = {}
-    for line in io.lines(os.getenv('HOME') .. '/.config/nvim/command-palette.txt') do
-        local items = line:split(':')
-        commands[items[1]:trim()] = items[2]:trim()
+    for line in io.lines(os.getenv('USERPROFILE') .. '/.config/nvim/command-palette.txt') do
+        local i, j = line:find(':')
+        local name = line:sub(1, i - 1)
+        local command = line:sub(j + 1)
+        commands[name:trim()] = command:trim()
     end
     vim.api.nvim_set_var('commandPalette', commands)
 end
