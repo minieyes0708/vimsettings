195593d21a8cb16584794a7352dabe25bc1b42c0
Minieyes Chen <chenvey2@gmail.com>
2023-12-14 22:59:49 +0800

    check command-palette.txt and colon in each line

--- minilua/command-palette.lua
+++ minilua/command-palette.lua
@@ -4,11 +4,18 @@ local this = {}
 
 function this.update()
     local commands = {}
-    for line in io.lines(os.getenv('USERPROFILE') .. '/.config/nvim/command-palette.txt') do
-        local i, j = line:find(':')
-        local name = line:sub(1, i - 1)
-        local command = line:sub(j + 1)
-        commands[name:trim()] = command:trim()
+    local filename = os.getenv('USERPROFILE') .. '/.config/nvim/command-palette.txt'
+    local file = io.open(filename, 'r')
+    if file then
+        file:close()
+        for line in io.lines(filename) do
+            local i, j = line:find(':')
+            if i then
+                local name = line:sub(1, i - 1)
+                local command = line:sub(j + 1)
+                commands[name:trim()] = command:trim()
+            end
+        end
     end
     vim.api.nvim_set_var('commandPalette', commands)
 end
